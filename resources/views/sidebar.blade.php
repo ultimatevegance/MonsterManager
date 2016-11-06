23387ccf61c87c3259b2b12a9829c5585d64d4fc
MonsterSucker <martinvegance@live.com>
2016-11-06 17:40:33 +0800

    UI

---
+++ resources/views/sidebar.blade.php
@@ -0,0 +1,60 @@
+  <aside class="main-sidebar">
+
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="{{ asset("/bower_components/admin-lte/dist/img/MineAvatar.jpg") }}" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Justus Woolworth</p>
+          <!-- Status -->
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+
+      <!-- search form (Optional) -->
+      <form action="#" method="get" class="sidebar-form">
+        <div class="input-group">
+          <input type="text" name="q" class="form-control" placeholder="Search...">
+              <span class="input-group-btn">
+                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                </button>
+              </span>
+        </div>
+      </form>
+      <!-- /.search form -->
+
+      <!-- Sidebar Menu -->
+      <ul class="sidebar-menu">
+        <li class="header">Admin Panel</li>
+        <!-- Optionally, you can add icons to the links -->
+        <li class="active"><a href="#"><i class="glyphicon glyphicon-home"></i> <span>DashBoard</span></a></li>
+        <li><a href="#"><i class="glyphicon glyphicon-th-large"></i> <span>Fearture</span></a></li>
+        <li><a href="#"><i class="glyphicon glyphicon-envelope"></i> <span>Emails</span></a></li>
+        <li><a href="#"><i class="glyphicon glyphicon-xbt"></i> <span>Finance</span></a></li>
+        <li><a href="#"><i class="glyphicon glyphicon-briefcase"></i> <span>Business</span></a></li>
+        <li><a href="#"><i class="glyphicon glyphicon-phone"></i> <span>Mobile</span></a></li>
+        <li><a href="#"><i class="glyphicon glyphicon-user"></i> <span>Profile Setting</span></a></li>
+        <li><a href="#"><i class="glyphicon glyphicon-calendar"></i> <span>Calendar</span></a></li>
+        <li><a href="#"><i class="glyphicon glyphicon-comment
+glyphicon "></i> <span>Feedback</span></a></li>
+
+        <li class="treeview">
+          <a href="#"><i class="fa fa-cog"></i> <span>System</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#">Mobile App Management</a></li>
+            <li><a href="#">Feedback</a></li>
+          </ul>
+        </li>
+      </ul>
+      <!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+  </aside>
